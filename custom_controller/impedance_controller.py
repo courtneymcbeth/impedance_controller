55defebfbb6a87927eeadddcdd78c5e622bb6e94
Courtney McBeth <cmcbeth2@illinois.edu>
2024-08-24 14:31:52 -0500

    Updating servo version

--- custom_controller/impedance_controller.py
+++ custom_controller/impedance_controller.py
@@ -1,243 +1,129 @@
-import numpy as np
-from scipy.spatial.transform import Rotation as R
 import rclpy
 from rclpy.node import Node
-from std_msgs.msg import String
-from sensor_msgs.msg import JointState
-from geometry_msgs.msg import PoseStamped, WrenchStamped, Pose
-from trajectory_msgs.msg import JointTrajectory, JointTrajectoryPoint
-from builtin_interfaces.msg import Duration
-from numpy.linalg import norm, solve
-import pinocchio as pin
-import os
-from ament_index_python.packages import get_package_share_directory
+from moveit_msgs.srv import ServoCommandType
+from geometry_msgs.msg import WrenchStamped, TwistStamped, Vector3
+from tf2_ros import Buffer, TransformListener, LookupException, ConnectivityException, ExtrapolationException
+from scipy.spatial.transform import Rotation as R
+import numpy as np
+import math
 
 class ImpedanceController(Node):
     def __init__(self):
         super().__init__('impedance_controller')
 
-        # Subscribe to the joint states
-        self.joint_state_subscription = self.create_subscription(
-            JointState,
-            '/joint_states',
-            self.joint_state_callback,
-            10)
-
         # Subscribe to the force-torque sensor data
         self.force_torque_subscription = self.create_subscription(
             WrenchStamped,
             '/force_torque_sensor_broadcaster/wrench',
-            self.force_torque_callback,
+            self.wrench_callback,
             10)
 
-        # Publisher for the joint trajectory and end-effector pose
-        self.ee_pose_publisher = self.create_publisher(PoseStamped, '/ee_pose', 10)
-        self.joint_trajectory_publisher = self.create_publisher(JointTrajectory, '/scaled_joint_trajectory_controller/joint_trajectory', 10)
+        # Compliance parameters
+        self.Kp = 1.0  # Stiffness (inverse of compliance)
+        self.Kd = 0.1  # Damping
 
-        # Impedance control parameters
-        self.mass_matrix = np.diag([5.0, 5.0, 5.0, 0.5, 0.5, 0.5])
-        self.stiffness_matrix = np.diag([300.0, 300.0, 300.0, 30.0, 30.0, 30.0])
-        self.damping_matrix = np.diag([15.0, 15.0, 15.0, 1.5, 1.5, 1.5])
+        self.current_twist = TwistStamped()  # Keep track of the current twist for damping effect
 
-        # Current state
-        # self.current_transform = np.eye(4)  # 4x4 identity matrix as initial transform
-        self.current_transform = None
-        self.current_velocity = np.zeros(6)  # 6D velocity: linear (x, y, z) and angular (roll, pitch, yaw)
+        # TF Buffer and Listener to get transformations
+        self.tf_buffer = Buffer()
+        self.tf_listener = TransformListener(self.tf_buffer, self)
 
-        # Force-torque data
-        self.force_torque = np.zeros(6)
+        # Publisher for the computed Twist
+        self.twist_pub_ = self.create_publisher(TwistStamped, '/servo_node/delta_twist_cmds', 10)
 
-        # Current joint states
-        self.current_joint_positions = None
-        self.current_joint_velocities = None
-        self.initialized = False
+        # Set the rate to 500 Hz
+        self.timer = self.create_timer(1.0 / 500.0, self.timer_callback)
 
-        # Declare parameters
-        self.declare_parameter('robot_description', '')
-        self.declare_parameter('base_link', 'base_link')
-        self.declare_parameter('end_effector_link', 'tool0')
+        self.latest_wrench = None
 
-        # Get URDF from the parameter server
-        self.urdf = self.get_parameter('robot_description').value
+        # Create a client for the ServoCommandType service
+        self.switch_input_client = self.create_client(ServoCommandType, '/servo_node/switch_command_type')
 
-        urdf_file = os.path.join(get_package_share_directory('custom_controller'), 'urdf', 'ur5e.urdf')
-        self.robot_model = pin.buildModelFromUrdf(urdf_file)
-        self.robot_data = self.robot_model.createData()
-        self.ee_frame_id = self.robot_model.getFrameId('tool0')  # Replace with actual frame name
+        # Call the service to enable TWIST command type
+        self.enable_twist_command()
 
-        self.eps = 1e-4
-        self.IT_MAX = 1000
-        self.DT = 1e-1
-        self.damp = 1e-12
+    def enable_twist_command(self):
+        if not self.switch_input_client.wait_for_service(timeout_sec=1.0):
+            self.get_logger().warn('Service not available, waiting again...')
+            return
 
+        request = ServoCommandType.Request()
+        request.command_type = ServoCommandType.Request.TWIST
 
-    def joint_state_callback(self, msg):
-        """Callback to update the current joint states."""
-        self.current_joint_positions = np.roll(np.array(msg.position), 1)
-        self.current_joint_velocities = np.roll(np.array(msg.velocity), 1)  # Obtain joint velocities
-
-        self.compute_forward_kinematics()
-    
-    def compute_forward_kinematics(self):
-        """Compute the end-effector position using forward kinematics."""
-        if self.current_joint_positions is not None:
-            pin.forwardKinematics(self.robot_model, self.robot_data, self.current_joint_positions)
-            ee_pose = pin.updateFramePlacement(self.robot_model, self.robot_data, self.ee_frame_id)
-            # print("ee_pose: ", ee_pose)
-            self.current_transform = ee_pose
-
-            pos = ee_pose.translation
-            ori = pin.Quaternion(ee_pose.rotation).normalize()
-            self.publish_ee_pose(pos, ori)
-
-
-    def force_torque_callback(self, msg):
-        """Callback to update the force-torque data from the sensor."""
-        self.force_torque = np.array([msg.wrench.force.x,
-                                      msg.wrench.force.y,
-                                      msg.wrench.force.z,
-                                      msg.wrench.torque.x,
-                                      msg.wrench.torque.y,
-                                      msg.wrench.torque.z])
-
-        # Nullify small force/torque values to avoid noise-induced motions
-        self.force_torque = np.where(np.abs(self.force_torque) < 3.0, 0.0, self.force_torque)
-        ft_str = np.array2string(self.force_torque)
-        self.get_logger().info(f'ft: {ft_str}')
-
-        # Update control if joint positions are available
-        if self.current_joint_positions is not None:
-            self.update_control()
-
-    def update_control(self):
-        """Calculate the compliance control output and apply it."""
-        time_step = 0.1  # Time step for integration, adjust as needed
-        old = self.current_transform
-        transform = self.current_transform
+        future = self.switch_input_client.call_async(request)
+        rclpy.spin_until_future_complete(self, future)
         
-        old_str = np.array2string(old.translation)
-
-        # Calculate acceleration based on the applied force/torque
-        # acceleration = np.linalg.inv(self.mass_matrix) @ (
-        #     self.force_torque - self.damping_matrix @ self.current_velocity - self.stiffness_matrix @ self.current_transform[:3, 3])
-        acceleration = np.linalg.inv(self.mass_matrix) @ (
-            self.force_torque - self.damping_matrix @ self.current_velocity)
-
-        # Calculate the change in velocity
-        delta_velocity = acceleration * time_step
-
-        # Update the current velocity
-        self.current_velocity += delta_velocity
-        vel_str = np.array2string(self.current_velocity)
-        # self.get_logger().info(f'vel: {vel_str}')
-
-        # Integrate velocity to update position
-        delta_position = self.current_velocity[:3] * time_step
-        delta_rotation =self.current_velocity[3:] * time_step
-
-        # Update the current transformation matrix
-        # self.current_transform[:3, 3] += delta_position
-        # self.current_transform[:3, :3] = delta_rotation @ self.current_transform[:3, :3]
-        transform.translation += delta_position
-        # transform.rotation = delta_rotation @ transform.rotation
-        
-
-        # pos_str = np.array2string(transform.translation)
-        # self.get_logger().info(f'old pos: {old_str}')
-        # self.get_logger().info(f'new pos: {pos_str}')
-
-        # pos_str = np.array2string(old.translation - transform.translation)
-        self.get_logger().info(f'diff: {norm(old.translation - transform.translation, 2)}')
-
-
-        # Check if the force is below a certain threshold, implying no external force
-        self.get_logger().info(f'torque norm: {np.linalg.norm(self.force_torque, 2)}')
-        if np.linalg.norm(self.force_torque) < 3.0:
-            self.get_logger().info('HERE')
-            # If no significant force is applied, reset the velocity to zero
-            self.current_velocity = np.zeros(6)
-
-        # Calculate the inverse kinematics to get the joint positions 
-        joint_positions = self.solve_inverse_kinematics(transform)
-
-        # Publish the desired joint positions
-        self.publish_joint_positions(joint_positions)
-
-    def solve_inverse_kinematics(self, desired_transform):
-        """Calculate the inverse kinematics using the full transformation matrix."""
-
-        q_init = self.current_joint_positions
-        target_placement = pin.SE3(desired_transform.rotation, desired_transform.translation)  # Only position, no rotation
-
-        if norm(self.current_transform.translation-desired_transform.translation) < self.eps:
-            success, q_sol = True, q_init
+        if future.result() is not None and future.result().success:
+            self.get_logger().info('Switched to input type: TWIST')
         else:
-            # Solve IK
-            success, q_sol = self.compute_joint_placement(self.robot_model, self.robot_data, self.ee_frame_id, target_placement, q_init)
+            self.get_logger().warn('Could not switch input to: TWIST')
 
-        if success:
-            return q_sol
+    def wrench_callback(self, msg):
+        self.latest_wrench = msg
+
+    def timer_callback(self):
+        if self.latest_wrench is not None:
+            try:
+                # Look up the transformation from ft_frame to tool0 and then tool0 to base_link
+                ft_to_tool0 = self.tf_buffer.lookup_transform('tool0', self.latest_wrench.header.frame_id, rclpy.time.Time())
+                # tool0_to_base_link = self.tf_buffer.lookup_transform('base_link', 'tool0', rclpy.time.Time())
+
+                # Transform the force/torque from ft_frame to tool0
+                force = self.transform_vector(ft_to_tool0, self.latest_wrench.wrench.force)
+                torque = self.transform_vector(ft_to_tool0, self.latest_wrench.wrench.torque)
+
+                # Transform the force/torque from tool0 to base_link
+                # force = self.transform_vector(tool0_to_base_link, force)
+                # torque = self.transform_vector(tool0_to_base_link, torque)
+
+                # Nullify force/torque readings with magnitude < 3
+                force = self.nullify_small_magnitudes(force, 3.0)
+                torque = self.nullify_small_magnitudes(torque, 3.0)
+
+                # Compute the twist in base_link frame
+                twist = TwistStamped()
+                twist.header.stamp = self.get_clock().now().to_msg()
+                twist.header.frame_id = 'tool0'
+
+                twist.twist.linear.x = (1 / self.Kp) * force.x - self.Kd * self.current_twist.twist.linear.x
+                twist.twist.linear.y = (1 / self.Kp) * force.y - self.Kd * self.current_twist.twist.linear.y
+                twist.twist.linear.z = (1 / self.Kp) * force.z - self.Kd * self.current_twist.twist.linear.z
+
+                twist.twist.angular.x = (1 / self.Kp) * torque.x - self.Kd * self.current_twist.twist.angular.x
+                twist.twist.angular.y = (1 / self.Kp) * torque.y - self.Kd * self.current_twist.twist.angular.y
+                twist.twist.angular.z = (1 / self.Kp) * torque.z - self.Kd * self.current_twist.twist.angular.z
+
+                # Update the current twist for the next callback
+                self.current_twist = twist
+
+                # Publish the computed twist
+                self.twist_pub_.publish(twist)
+
+            except (LookupException, ConnectivityException, ExtrapolationException) as e:
+                self.get_logger().warn(f"Could not transform wrench to base_link frame: {str(e)}")
+
+    def transform_vector(self, transform, vector):
+        # Extract rotation (quaternion) and translation from TransformStamped
+        q = transform.transform.rotation
+
+        # Convert quaternion to rotation matrix using scipy
+        r = R.from_quat([q.x, q.y, q.z, q.w])
+
+        # Convert Vector3 to numpy array for easy multiplication
+        vector_np = np.array([vector.x, vector.y, vector.z])
+
+        # Apply the rotation
+        rotated_vector = r.apply(vector_np)
+
+        # Return the transformed vector as a Vector3
+        return Vector3(x=rotated_vector[0], y=rotated_vector[1], z=rotated_vector[2])
+
+    def nullify_small_magnitudes(self, vector, threshold):
+        magnitude = math.sqrt(vector.x ** 2 + vector.y ** 2 + vector.z ** 2)
+        if magnitude < threshold:
+            return Vector3(x=0.0, y=0.0, z=0.0)
         else:
-            self.get_logger().warn("IK solver did not converge to a solution.")
-            return q_init  # Return initial guess if IK fails
-
-    def compute_joint_placement(self, model, data, ee_frame_id, target_placement, q_init):
-        q = q_init
-        # print("q: ", q)
-        i = 0
-        while True:
-            pin.forwardKinematics(model, data, q)
-            iMd = data.oMf[ee_frame_id].actInv(target_placement)
-            err = pin.log(iMd).vector
-            if norm(err) < self.eps:
-                success = True
-                break
-            if i >= self.IT_MAX:
-                success = False
-                break
-            J = pin.computeFrameJacobian(model, data, q, ee_frame_id)
-            J = -np.dot(pin.Jlog6(iMd.inverse()), J)
-            v = -J.T.dot(solve(J.dot(J.T) + self.damp * np.eye(6), err))
-            q = pin.integrate(model, q, v * self.DT)
-            # if not i % 10:
-            #     print("%d: error = %s" % (i, err.T))
-            i += 1
-        if success:
-            print("Convergence achieved!")
-        else:
-            print(
-                "\nWarning: the iterative algorithm has not reached convergence to the desired precision"
-            )
-        return success, q
-
-    def publish_joint_positions(self, joint_positions):
-        """Publish the desired joint positions as a JointTrajectory message."""
-        trajectory_msg = JointTrajectory()
-        trajectory_msg.joint_names = ["shoulder_pan_joint", "shoulder_lift_joint", "elbow_joint", "wrist_1_joint", "wrist_2_joint", "wrist_3_joint"]
-
-        point = JointTrajectoryPoint()
-        if joint_positions is not None:
-            point.positions = joint_positions.tolist()
-        duration = Duration()
-        duration.nanosec = 1000000
-        point.time_from_start = duration
-
-        trajectory_msg.points = [point]
-        self.joint_trajectory_publisher.publish(trajectory_msg)
-
-    def publish_ee_pose(self, position, orientation):
-        """Publish the current end-effector pose."""
-        pose_msg = PoseStamped()
-        pose_msg.header.frame_id = 'world'
-        pose_msg.pose.position.x = position[0]
-        pose_msg.pose.position.y = position[1]
-        pose_msg.pose.position.z = position[2]
-        pose_msg.pose.orientation.x = orientation[0]
-        pose_msg.pose.orientation.y = orientation[1]
-        pose_msg.pose.orientation.z = orientation[2]
-        pose_msg.pose.orientation.w = orientation[3]
-
-        self.ee_pose_publisher.publish(pose_msg)
+            return vector
 
 def main(args=None):
     rclpy.init(args=args)
